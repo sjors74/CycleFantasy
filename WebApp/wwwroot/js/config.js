1f912e25e6de001a75283c769ac6365d0d7aec32
Sjors Stevens <sjors@rebelit.nl>
2025-05-09 10:26:31 +0200

    api base url based on hostname

--- WebApp/wwwroot/js/config.js
+++ WebApp/wwwroot/js/config.js
@@ -1,5 +1,10 @@
-﻿let API_BASE_URL = "https://webcycleapi20250508145015.azurewebsites.net";
-//let API_BASE_URL = "https://localhost:44302"
+﻿let API_BASE_URL = "";
+
+if (window.location.hostname === "localhost") {
+    API_BASE_URL = "https://localhost:44302";
+} else {
+    API_BASE_URL = "https://webcycleapi20250508145015.azurewebsites.net";
+}
 
 async function loadConfig() {
     const response = await fetch(`${API_BASE_URL}/config`);
