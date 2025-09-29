8ac28a00fe62d84910e3952ec4004cf43c817562
Sjors Stevens <sjors.stevens@digitall-health.nl>
2025-09-29 22:04:54 +0200

    clean up stages

--- WebCycleManager/Controllers/StagesController.cs
+++ WebCycleManager/Controllers/StagesController.cs
@@ -1,101 +1,24 @@
 ﻿using CycleManager.Services;
 using CycleManager.Services.Interfaces;
-using DataAccessEF.Migrations;
 using Domain.Context;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System.Linq;
 using WebCycleManager.Models;
 
 namespace WebCycleManager.Controllers
 {
     public class StagesController : Controller
     {
-        private readonly ApplicationDbContext _context;
         private readonly IStageService _stageService;
         private readonly IEventService _eventService;
 
-        public StagesController(ApplicationDbContext context, IStageService stageService, IEventService eventService)
+        public StagesController(IStageService stageService, IEventService eventService)
         {
-            _context = context;
             _stageService = stageService;
             _eventService = eventService;
         }
-
-        // GET: Stages
-        public async Task<IActionResult> Index(int searchEventId)
-        {
-            var vm = new StagesViewModel();
-            vm.CurrentSearchEventId = searchEventId;
-            var events = await _context.Events.OrderBy(e => e.EventName).ToListAsync();
-            var eventSelectList = new List<SelectListItem>();
-            foreach(var e in events)
-            {
-                eventSelectList.Add(new SelectListItem { Text = string.Concat(e.EventName, " ", e.EventYear), Value = e.EventId.ToString() });
-            }
-            vm.Events = eventSelectList;
-            var stages = _context.Stages;
-
-            //if search filter has value, return filtered stage list
-            if (searchEventId > 0)
-            {
-                var stagesDb = await stages.Where(e => e.EventId == searchEventId).ToListAsync();
-                foreach(var s in stagesDb)
-                {
-                    vm.Stages.Add(CreateViewModel(s));
-                }    
-                return View(vm);
-            }
-
-
-            foreach (var s in stages)
-            {
-                vm.Stages.Add(CreateViewModel(s));
-            }
-
-            return View(vm);
-        }
-
-        // GET: Stages/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Stages == null)
-            {
-                return NotFound();
-            }
-
-            var stage = await _context.Stages
-                .Include(e => e.Event)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (stage == null)
-            {
-                return NotFound();
-            }
-
-            var vm = CreateViewModel(stage);
-
-            return View(vm);
-        }
-
-        // GET: Stages/Create
-        //public IActionResult Create()
-        //{
-        //    ViewData["EventId"] = new SelectList(
-        //        _context.Events
-        //            .Select(e => new {
-        //                e.EventId,
-        //                Text = e.EventName + " (" + e.EventYear + ")"
-        //            })
-        //            .ToList(),
-        //        "EventId",
-        //        "Text"
-        //    );
-
-        //    return View();
-        //}
 
         [HttpGet]
         public async Task<IActionResult> Create()
@@ -164,92 +87,12 @@ namespace WebCycleManager.Controllers
                 EventId = stage.EventId
             };
 
-            _context.Add(entity);
-            await _context.SaveChangesAsync();
+            await _stageService.AddStage(entity);
 
-            // Succes: return JSON
             return Json(new 
-            { 
-                success = true, 
+            {                 success = true, 
                 redirectUrl = Url.Action("Edit", "Events", new { id = stage.EventId })
             });
-        }
-            
-
-        // GET: Stages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Stages == null)
-            {
-                return NotFound();
-            }
-
-            var stage = await _context.Stages.FindAsync(id);
-            if (stage == null)
-            {
-                return NotFound();
-            }
-            var vm = CreateViewModel(stage);
-            ViewData["EventId"] = new SelectList(
-                _context.Events
-                    .Select(e => new {
-                        e.EventId,
-                        Text = e.EventName + " (" + e.EventYear + ")"
-                    })
-                    .ToList(),
-                "EventId",
-                "Text",
-                stage.EventId
-            );
-            return View(vm);
-        }
-
-        // POST: Stages/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("StageId,StageDate,StageName,StageOrder,StartLocation,FinishLocation,NoScore,NoScoreDescription,EventId")] StageViewModel stage)
-        {
-            if (id != stage.StageId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    var s = CreateFromViewModel(stage);
-
-                    _context.Update(s);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StageExists(stage.StageId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["EventId"] = new SelectList(
-                _context.Events
-                    .Select(e => new {
-                        e.EventId,
-                        Text = e.EventName + " (" + e.EventYear + ")"
-                    })
-                    .ToList(),
-                "EventId",
-                "Text",
-                stage.EventId
-            );
-            return View(stage);
         }
 
         [HttpGet]
@@ -304,55 +147,12 @@ namespace WebCycleManager.Controllers
             });
         }
 
-        // GET: Stages/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Stages == null)
-            {
-                return NotFound();
-            }
-
-            var stage = await _context.Stages
-                .Include(s => s.Event)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (stage == null)
-            {
-                return NotFound();
-            }
-            var vm = CreateViewModel(stage);
-            return View(vm);
-        }
-
-        // POST: Stages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Stages == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Stages'  is null.");
-            }
-            var stage = await _context.Stages.FindAsync(id);
-            if (stage != null)
-            {
-                _context.Stages.Remove(stage);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteAjax(int id)
         {
             bool deleted = await _stageService.DeleteStage(id);
             return Json(new { success = deleted });
-        }
-
-        private bool StageExists(int id)
-        {
-          return (_context.Stages?.Any(e => e.Id == id)).GetValueOrDefault();
         }
 
         public StageViewModel CreateViewModel(Stage stage)
@@ -374,11 +174,9 @@ namespace WebCycleManager.Controllers
             return vm;
         }
 
-        public Stage CreateFromViewModel(StageViewModel vm)
+        public async Task<Stage> CreateFromViewModel(StageViewModel vm)
         {
-            var stage = _context.Stages
-                .Include(s => s.Event)
-                .FirstOrDefault(s => s.Id == vm.StageId);
+            var stage = await _stageService.GetStageById(vm.StageId);
 
             try
             {
@@ -404,7 +202,7 @@ namespace WebCycleManager.Controllers
         }
         private async Task<IEnumerable<SelectListItem>> GetEventSelectListAsync()
         {
-            return await _context.Events
+            return _eventService.GetAllEvents().Result
                 .OrderByDescending(e => e.EventYear)
                 .ThenBy(e => e.StartDate)
                 .Select(e => new SelectListItem
@@ -412,7 +210,7 @@ namespace WebCycleManager.Controllers
                     Value = e.EventId.ToString(),
                     Text = e.EventName + " (" + e.EventYear + ")"
                 })
-                .ToListAsync();
+                .ToList();
         }
     }
 }
