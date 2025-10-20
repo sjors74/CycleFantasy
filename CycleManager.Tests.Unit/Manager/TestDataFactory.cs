72a2f5bf2157a37a15c720b4d68fcc84b7b2186a
Sjors Stevens <sjors.stevens@digitall-health.nl>
2025-10-20 23:38:24 +0200

    CompetitorController tests wip

--- CycleManager.Tests.Unit/Manager/TestDataFactory.cs
+++ CycleManager.Tests.Unit/Manager/TestDataFactory.cs
@@ -1,4 +1,6 @@
-﻿using CycleManager.Domain.Models;
+﻿using CycleManager.Domain.Dto;
+using CycleManager.Domain.Models;
+using Domain.Dto;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WebCycleManager.Models;
@@ -116,6 +118,146 @@ namespace CycleManager.Tests.Unit.Manager
                 FinishLocation = stage.FinishLocation,
                 NoScore = stage.NoScore,
                 NoScoreDescription = stage.NoScoreDescription
+            };
+        }
+
+        public static List<CompetitorDto> CreateCompetitorDtos(int count)
+        {
+            return Enumerable.Range(1, count)
+                .Select(i => new CompetitorDto
+                {
+                    CompetitorId = i,
+                    FirstName = $"Rider{i}",
+                    LastName = $"Lastname{i}",
+                    PcsName = $"PCS{i}", 
+                    CountryShort = $"Country{i}", 
+                    CurrentTeamName = $"Team{i}"
+                })
+                .ToList();
+        }
+
+        public static Competitor CreateCompetitor()
+        {
+            return new Competitor
+            {
+                CompetitorId = 1,
+                FirstName = "Remco",
+                LastName = "Evenepoel",
+                CountryId = 1,
+                PcsName = "EvenepoelR",
+                Country = new Country
+                {
+                    CountryId = 1,
+                    CountryNameLong = "Belgium"
+                },
+                CompetitorInTeams = new List<CompetitorInTeam>()
+            };
+        }
+
+        public static List<Competitor> CreateCompetitors(int count)
+        {
+            return Enumerable.Range(1, count)
+                .Select(i => new Competitor
+                {
+                    CompetitorId = i,
+                    FirstName = $"First{i}",
+                    LastName = $"Last{i}",
+                    CountryId = i,
+                    Country = new Country
+                    {
+                        CountryId = i,
+                        CountryNameLong = $"Country{i}"
+                    }
+                }).ToList();
+        }
+
+        public static Competitor CreateCompetitorWithTeam()
+        {
+            var team = new Team
+            {
+                TeamId = 1,
+                CurrentTeamName = "Soudal Quick-Step"
+            };
+
+            var competitor = new Competitor
+            {
+                CompetitorId = 1,
+                FirstName = "Remco",
+                LastName = "Evenepoel",
+                Country = new Country
+                {
+                    CountryId = 1,
+                    CountryNameLong = "Belgium"
+                },
+                PcsName = "EvenepoelR",
+                CompetitorInTeams = new List<CompetitorInTeam>
+                {
+                    new CompetitorInTeam
+                    {
+                        Id = 1,
+                        CompetitorId = 1,
+                        TeamId = 1,
+                        Year = DateTime.Now.Year,
+                        Team = team,
+                        IsNationalChampion = true
+                    }
+                }
+            };
+
+            team.CompetitorInTeams = new List<CompetitorInTeam> { competitor.CompetitorInTeams.First() };
+
+            return competitor;
+        }
+
+        public static CreateCompetitorViewModel CreateValidCreateCompetitorViewModel()
+        {
+            return new CreateCompetitorViewModel
+            {
+                CompetitorId = 0,
+                FirstName = "Wout",
+                LastName = "Van Aert",
+                PcsName = "VanAertW",
+                CountryId = 1,
+                TeamId = 1,
+                Year = DateTime.Now.Year,
+                IsNationalChampion = false
+            };
+        }
+
+        public static CompetitorEditDto CreateCompetitorEditDto()
+        {
+            return new CompetitorEditDto
+            {
+                CompetitorId = 1,
+                FirstName = "Tadej",
+                LastName = "Pogacar",
+                PcsName = "PogacarT",
+                ScraperName = "TadejP",
+                CountryId = 1,
+                SelectedTeamId = 1,
+                SelectedYear = DateTime.Now.Year,
+                Countries = new List<CountryDto>
+                {
+                    new CountryDto { Id = 1, CountryNameLong = "Slovenia" },
+                    new CountryDto { Id = 2, CountryNameLong = "Belgium" }
+                },
+                Teams = new List<TeamDto>
+                {
+                    new TeamDto { Id = 1, Naam = "UAE Team Emirates", Renners = new List<CompetitorDto>() },
+                    new TeamDto { Id = 2, Naam = "Jumbo-Visma", Renners = new List<CompetitorDto>() }
+                },
+                AvailableYears = new List<int> { 2023, 2024, 2025 },
+                CompetitorInTeams = new List<CompetitorInTeamDto>
+                {
+                    new CompetitorInTeamDto
+                    {
+                        CompetitorInTeamId = 1,
+                        TeamId = 1,
+                        Year = 2024,
+                        IsNationalChampion = false,
+                        TeamName = "UAE Team Emirates"
+                    }
+                }
             };
         }
     }
