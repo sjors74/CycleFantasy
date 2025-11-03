0f144d2128c34d28473e139251468bc1ce86f246
Sjors Stevens <sjors.stevens@digitall-health.nl>
2025-11-03 15:28:09 +0100

    unit tests voor newscontroller (manager)

--- CycleManager.Services/NewsService.cs
+++ CycleManager.Services/NewsService.cs
@@ -12,9 +12,34 @@ namespace CycleManager.Services
             _repository = repository;
         }
 
-        public Task<IEnumerable<NewsItem>> GetAllActiveNewsItems()
+        public async Task CreateAsync(NewsItem item)
         {
-            return _repository.GetAllActiveNewsItems();
+            await _repository.CreateAsync(item);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _repository.ExistsAsync(id);
+        }
+
+        public async Task<IEnumerable<NewsItem>> GetAllActiveNewsItems()
+        {
+            return await _repository.GetAllActiveNewsItems();
+        }
+
+        public async Task<NewsItem?> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(NewsItem item)
+        {
+            await _repository.UpdateAsync(item);
         }
     }
 }
