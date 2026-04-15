148d9286732d022607f0d20c316d9a69c5db0feb
Sjors Stevens <sjors.stevens@digitall-health.nl>
2026-04-15 13:29:24 +0200

    update to fix bug

--- CycleManager.Tests.Integration/Helpers/AutoMapperTestHelper.cs
+++ CycleManager.Tests.Integration/Helpers/AutoMapperTestHelper.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Domain.Mapping;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace CycleManager.Tests.Helpers
 {
@@ -7,10 +9,13 @@ namespace CycleManager.Tests.Helpers
     {
         public static IMapper CreateMapper()
         {
+            using var loggerFactory = LoggerFactory.Create(_ => { });
+
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.AddMaps(typeof(DomainToResponseMappingProfile).Assembly);
-            });
+                cfg.AddProfile<DomainToResponseMappingProfile>();
+            }, loggerFactory);
+
             config.AssertConfigurationIsValid();
             return config.CreateMapper();
         }
